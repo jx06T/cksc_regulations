a5d0b81f632bd08decaefa74c4cde7fadcace5bd
jx06T <50313tjx06@gmail.com>
2024-10-02 15:46:33 +0800

    DDD

--- src/starlight/locals.ts
+++
@@ -1,8 +0,0 @@
-import { defineMiddleware } from 'astro:middleware';
-import { useTranslations } from './utils/translations';
-
-export const onRequest = defineMiddleware((context, next) => {
-	context.locals.t = useTranslations(context.currentLocale);
-
-	return next();
-});
