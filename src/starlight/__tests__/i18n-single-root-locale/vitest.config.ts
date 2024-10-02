5fe75a349d2d53ed07399945c96cfce95892ce0e
jx06T <50313tjx06@gmail.com>
2024-10-02 15:40:24 +0800

    testD

--- src/starlight/__tests__/i18n-single-root-locale/vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineVitestConfig } from '../test-config';
-
-export default defineVitestConfig({
-	title: 'i18n with a single root locale',
-	locales: {
-		root: { label: 'Français', lang: 'fr-CA' },
-	},
-});
--- src/starlight/__tests__/sidebar-slug-error/vitest.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineVitestConfig } from '../test-config';
-
-export default defineVitestConfig({
-	title: 'sidebar slug error',
-	sidebar: ['/getting-started/'],
-});
